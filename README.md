63c49ba46850e3c03fcc52fcb5d60c6b3d7a0981
Boris Cherny <bcherny@gmail.com>
2016-02-06 18:59:33 -0800

    learner, with tree pruning

--- README.md
+++ README.md
@@ -8,3 +8,165 @@
 npm i
 npm start
 ```
+
+### sample output (from test case 2)
+
+```text
+~/Sites/github/learner (master)*$ node learner.js
+learned: a > b (addChilding b to a to root)
+  world: root
+└── a
+    └── b
+        └── (empty)
+        
+    
+
+learned: a > e (addChilding to e to a)
+  world: root
+└── a
+    ├── b
+    │   └── (empty)
+    │   
+    └── e
+        └── (empty)
+        
+    
+
+learned: e > r (addChilding to r to e)
+  world: root
+└── a
+    ├── b
+    │   └── (empty)
+    │   
+    └── e
+        └── r
+            └── (empty)
+            
+        
+    
+
+learned: b > c (addChilding to c to b)
+  world: root
+└── a
+    ├── b
+    │   └── c
+    │       └── (empty)
+    │       
+    │   
+    └── e
+        └── r
+            └── (empty)
+            
+        
+    
+
+learned: c > d (addChilding to d to c)
+  world: root
+└── a
+    ├── b
+    │   └── c
+    │       └── d
+    │           └── (empty)
+    │           
+    │       
+    │   
+    └── e
+        └── r
+            └── (empty)
+            
+        
+    
+
+learned: a > o (addChilding to o to a)
+  world: root
+└── a
+    ├── b
+    │   └── c
+    │       └── d
+    │           └── (empty)
+    │           
+    │       
+    │   
+    ├── e
+    │   └── r
+    │       └── (empty)
+    │       
+    │   
+    └── o
+        └── (empty)
+        
+    
+
+learned: d > e (addChilding to e to d)
+  world: root
+└── a
+    ├── b
+    │   └── c
+    │       └── d
+    │           └── e
+    │               └── r
+    │                   └── (empty)
+    │                   
+    │               
+    │           
+    │       
+    │   
+    └── o
+        └── (empty)
+        
+    
+
+learned: e > r (addChilding to r to e)
+  world: root
+└── a
+    ├── b
+    │   └── c
+    │       └── d
+    │           └── e
+    │               └── r
+    │                   └── (empty)
+    │                   
+    │               
+    │           
+    │       
+    │   
+    └── o
+        └── (empty)
+        
+    
+
+learned: r > o (addChilding to o to r)
+  world: root
+└── a
+    └── b
+        └── c
+            └── d
+                └── e
+                    └── r
+                        └── o
+                            └── (empty)
+                            
+                        
+                    
+                
+            
+        
+    
+
+   done: root
+└── a
+    └── b
+        └── c
+            └── d
+                └── e
+                    └── r
+                        └── o
+                            └── (empty)
+                            
+                        
+                    
+                
+            
+        
+ 
+```
--- learner.ts
+++ learner.ts
@@ -15,24 +15,48 @@
 */
 
 const prettyTree = require('pretty-tree')
+const {without} = require('lodash')
 
 class TreeSet {
 
-  // TODO: split TreeSet into TreeSet and TreeSetNode
+  // TODO: is there a better way to implement this?
+  // maybe split TreeSet into TreeSet and TreeSetNode
   private static index = {}
 
-  constructor(public val: string, public children: TreeSet[] = []) {
+  constructor(
+    public val: string,
+    public children: TreeSet[] = [],
+    public parent?: TreeSet|void
+   ) {
     TreeSet.index[val] = this
   }
+
   public includesVal (a: string): boolean {
     return this.val === a || this.children.some(_ => _.includesVal(a))
   }
-  public getByVal (a: string): TreeSet|void {
+  public static getByVal (a: string): TreeSet|void {
     return TreeSet.index[a]
   }
-  public append (a: TreeSet): TreeSet {
-    // TODO: rm from an existing point in the tree, if it already exists
+  public getRoot(): TreeSet {
+    return this.parent
+      ? this.parent.getRoot()
+      : this
+  }
+  public removeChild (a: TreeSet): void {
+    this.children = without(this.children, a)
+  }
+  public addChild (a: TreeSet): TreeSet {
+
+    // rm, if it already exists
+    const existingNode = TreeSet.getByVal(a.val)
+    if (existingNode && existingNode.parent) {
+      existingNode.parent.removeChild(existingNode)
+    }
+
+    // add
+    a.parent = this
     this.children.push(a)
+
     return this
   }
   public toString (): string {
@@ -40,10 +64,10 @@ class TreeSet {
       ? `(${this.val} => ${this.children.map(_ => _.toString())})`
       : this.val
   }
-  public toPrettyPrintString () {
+  public toPrettyString () {
     return prettyTree({
       label: this.val,
-      nodes: this.children.map(_ => _.toPrettyPrintString())
+      nodes: this.children.map(_ => _.toPrettyString())
     })
   }
 }
@@ -52,26 +76,26 @@ class Learner {
   private state = {
     tree: new TreeSet('root')
   }
-  public getTree (): string {
-    return this.state.tree.toString()
-  }
   public isAmbiguous(): boolean {
-    return this.state.tree.children.length > 1 || this.state.tree.children.some(_ => _.children.length > 1)
+    return this.state.tree.children.length > 1
+        || this.state.tree.children.some(_ => _.children.length > 1)
   }
   public learn (a: string, b: string): void {
-    if ([a,b].every(this.state.tree.includesVal)) {
-      console.info(`learned: ${a} > ${b} (appending to ${b} to ${a})`)
-      this.state.tree.getByVal(b).append(this.state.tree.getByVal(a))
+    if ([a,b].every(this.state.tree.includesVal.bind(this.state.tree))) {
+      console.info(`learned: ${a} > ${b} (addChilding to ${b} to ${a})`)
+      TreeSet.getByVal(a).addChild(TreeSet.getByVal(b))
+    } else if (this.state.tree.includesVal(b)) {
+      // TODO: can this ever happen, assuming that training data arrives in order?
     } else if (this.state.tree.includesVal(a)) {
-      console.info(`learned: ${a} > ${b} (appending to ${b} to ${a})`)
-      this.state.tree.getByVal(a).append(new TreeSet(b))
+      console.info(`learned: ${a} > ${b} (addChilding to ${b} to ${a})`)
+      TreeSet.getByVal(a).addChild(new TreeSet(b))
     } else {
-      console.info(`learned: ${a} > ${b} (appending ${b} to ${a} to root)`)
-      const _a = new TreeSet(a)
-      _a.append(new TreeSet(b))
-      this.state.tree.getByVal('root').append(_a)
+      console.info(`learned: ${a} > ${b} (addChilding ${b} to ${a} to root)`)
+      this.state.tree.getRoot().addChild(
+        new TreeSet(a).addChild(new TreeSet(b))
+      )
     }
-    console.info(`  world: ${this.getTree()}`)
+    console.info(`  world: ${this.toPrettyString()}`)
   }
   public train (as: string[]): void {
     as.reduce((a, b) => {
@@ -85,8 +109,8 @@ class Learner {
       return b
     })
   }
-  public toPrettyPrintString(): string {
-    return this.state.tree.toPrettyPrintString()
+  public toPrettyString(): string {
+    return this.state.tree.toPrettyString()
   }
   public toString(): string {
     return this.state.tree.toString()
@@ -103,7 +127,7 @@ class Learner {
 //   'dad',
 //   'dog'
 // ])
-// console.log(`   done: ${a.toPrettyPrintString()}`)
+// console.log(`   done: ${a.toPrettyString()}`)
 // console.assert(a.isAmbiguous() === true)
 // console.assert(a.toString() === '(root => (a => (b => (c => d)),e,o))')
 
@@ -118,6 +142,9 @@ b.train([
   'dad',
   'dog',
   'elephant',
-  'ralph'
+  'ralph',
+  'orange'
 ])
-console.log(`   done: ${b.toPrettyPrintString()}`)
+console.log(`   done: ${b.toPrettyString()}`)
+console.assert(b.isAmbiguous() === false)
+console.assert(b.toString() === '(root => (a => (b => (c => (d => (e => (r => o)))))))')
